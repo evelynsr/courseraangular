9f2cec4a8dbdad3fea77f72858af0d2d5dc0581c
evelynsr <saroev24@gmail.com>
2017-11-11 19:54:25 -0600

    controladores

---
+++ module4-solution/src/controllers/categories.controller.js
@@ -0,0 +1,10 @@
+(function(){
+	'use strict'
+	angular.module('Data')
+	.controller('CategoriesController', CategoriesController);
+	CategoriesController.$inject = ['MenuDataService', 'request'];
+	function CategoriesController(MenuDataService, request){
+		var categories = this;
+		categories.items = request.data;
+	}
+})();
---
+++ module4-solution/src/controllers/menuItems.controller.js
@@ -0,0 +1,10 @@
+(function (){
+	'use strict';
+	angular.module('Data')
+	.controller('MenuItemsController', MenuItemsController);
+	MenuItemsController.$inject = ['MenuDataService', 'request'];
+	function MenuItemsController(MenuDataService, request){
+		var menu = this;
+		menu.items = request.data;
+	}
+})();
